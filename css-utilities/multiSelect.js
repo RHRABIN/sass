1714e4838a70c44ece5d244857d81c0fa0a850b2
RHRABIN <rhrabin934602@gmail.com>
2023-12-24 13:43:42 +0600

    project setup with input, select, button component style

---
+++ css-utilities/multiSelect.js
@@ -0,0 +1,73 @@
+const selected_item_parent = document.querySelector(".selected-item");
+const selected_items = document.querySelectorAll(".item");
+
+const handleSelect = (clickValue) => {
+    const items = document.querySelectorAll(".item");
+    const options = document.querySelectorAll(".option");
+
+    const selectedIds = Array.from(items).map(i => i.id.toLocaleLowerCase());
+
+    //check is exist or not
+    const isExist = selectedIds.find(i => i == clickValue.toLocaleLowerCase());
+
+    if (isExist == undefined) {
+        Array.from(options).forEach(i => {
+            if (i.innerText == clickValue) {
+                i.classList.add("selected")
+            }
+        });
+
+        const newDiv = document.createElement("div");
+        newDiv.id = clickValue;
+        newDiv.className = "item";
+        newDiv.innerHTML = `
+    ${clickValue}
+    <button onclick="removeSelectedItem(event, ${clickValue})">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+            <path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z" fill="currentColor"></path>
+        </svg>
+    </button>
+`;
+        selected_item_parent.insertBefore(newDiv, selected_item_parent.querySelector('.search-input'));
+
+    } else {
+        //  remove the selected class
+        Array.from(options).forEach(i => {
+            if (i.innerText == clickValue) {
+                i.classList.remove("selected")
+            }
+        });
+
+        //remove from selected list
+        filterData = Array.from(items).filter(i => i.id !== clickValue);
+        selected_item_parent.textContent = "";
+        Array.from(filterData).forEach(i => selected_item_parent.insertBefore(i, selected_item_parent.querySelector('.search-input')))
+
+    }
+    document.getElementById("search-placeholder").textContent = ""
+
+}
+
+const removeSelectedItem = (event, value) => {
+    event.stopPropagation();
+    const selectedItems = document.querySelectorAll(".item");
+
+    const restData = Array.from(selectedItems).filter(i => i.id.toLocaleLowerCase() !== value.id.toLocaleLowerCase());
+    const searchInput = selected_item_parent.querySelector('.search-input');
+
+    // Remove existing elements before the search input
+    while (selected_item_parent.firstChild !== searchInput) {
+        selected_item_parent.removeChild(selected_item_parent.firstChild);
+    }
+
+    // Insert the filtered elements before the search input
+    restData.map(el => selected_item_parent.insertBefore(el, searchInput));
+
+    //  set the search... text div
+    if (selectedItems.length <= 1 && !document.getElementById("search-placeholder")) {
+        const newDiv = document.createElement("div");
+        newDiv.id = "search-placeholder";
+        newDiv.innerHTML = `Search...`;
+        selected_item_parent.insertBefore(newDiv, selected_item_parent.querySelector('.search-input'));
+    }
+}
---
+++ css-utilities/select.js
@@ -0,0 +1,108 @@
+const optionMenu = document.querySelector(".select-group"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
+const search_input = document.getElementById("search-input");
+
+let isOptionOpen = false;
+
+const handleToggle = () => {
+    optionMenu.classList.toggle("active")
+    if (optionMenu.classList.contains("active")) {
+        search_input.focus()
+    }
+    isOptionOpen = true;
+}
+
+// which option is clicked and set the value and option class selected added
+// options.forEach(option => {
+//     option.addEventListener("click", () => {
+//         // Remove "selected" class from all options
+//         options.forEach(opt => opt.classList.remove("selected"));
+
+//         // Add "selected" class to the clicked option
+//         option.classList.add("selected");
+
+//         // Update the dropdown value and close the dropdown
+//         let selectedOption = option.querySelector(".option-text");
+//         // console.log(selectedOption.attributes.value.value)
+//         sBtn_text.value = selectedOption.innerText;
+//         optionMenu.classList.remove("active");
+//         isOptionOpen = !isOptionOpen;
+//     });
+// });
+
+// which item is active to catch this variable
+let active = -1;
+const dropdownMenuItems = Array.from(options || []);
+
+// Add a click event listener to the document to close the dropdown when clicking outside
+document.addEventListener("click", (event) => {
+    const isClickInside = optionMenu.contains(event.target) || selectBtn.contains(event.target);
+
+    if (!isClickInside && isOptionOpen) {
+        optionMenu.classList.remove("active");
+        isOptionOpen = false;
+    }
+});
+
+const handleOptionKeyDown = (e) => {
+    if (isOptionOpen) {
+        if (e.key == "ArrowDown") {
+            if (active < dropdownMenuItems.length - 1) {
+                active++
+                dropdownMenuItems[active].focus();
+            }
+        } else if (e.key == "ArrowUp") {
+            if (active > 0) {
+                active--
+                dropdownMenuItems[active].focus()
+            }
+        }
+        else if (e.key == "Enter") { // Enter key
+            if (active !== -1) {
+                // Remove "selected" class from all options
+                dropdownMenuItems.forEach(opt => opt.classList.remove("selected"));
+
+                let selectedOption = dropdownMenuItems[active].querySelector(".option-text");
+                dropdownMenuItems[active].classList.add("selected");
+                sBtn_text.value = selectedOption.innerText;
+                optionMenu.classList.remove("active");
+                isOptionOpen = !isOptionOpen;
+            }
+        }
+    }
+}
+
+//filter dropdown item
+
+const handleFilter = (e) => {
+    const searchText = e.target.value;
+    const itemsData = Array.from(options).map(item => item.innerText);
+    const filterData = itemsData.flatMap(i => {
+        if (i.toLowerCase().includes(searchText.toLowerCase())) {
+            return Array.from(options).filter(item =>
+                item.innerText.toLowerCase().includes(i.toLowerCase())
+            );
+        }
+        return [];
+    });
+
+    const op = document.querySelector(".options");
+    op.textContent = ""; // Clear the existing content
+
+    filterData.forEach(dataItem => {
+        op.appendChild(dataItem);
+    });
+};
+
+const handleClick = () => {
+    console.log("click")
+}
+
+const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+        optionMenu.classList.toggle("active")
+        isOptionOpen = true;
+    }
+}
